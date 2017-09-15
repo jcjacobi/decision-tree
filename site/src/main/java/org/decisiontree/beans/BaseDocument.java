d4f4b001e3dd0a91a396fe645d7a9c0244011f58
Jouke Jacobi <jouke.jacobi@bloomreach.com>
2017-09-15 10:43:32 +0200

    archetype 11.2.3

---
+++ site/src/main/java/org/decisiontree/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.decisiontree.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="decisiontree:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
